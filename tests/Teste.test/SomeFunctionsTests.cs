5e5c5b8996e064e77d7211feda5066e7b0d5b88e
Fabio Fonseca <ffonseca@jeracapital.com.br>
2025-01-05 19:43:22 -0300

    assertions

--- tests/Teste.test/SomeFunctionsTests.cs
+++ tests/Teste.test/SomeFunctionsTests.cs
@@ -3,59 +3,94 @@ using Teste.Bussiness;
 
 public class SomeFunctionsTests
 {
-    [Theory]
-    [InlineData(1, 1, 2)]
-    [InlineData(2, 2, 4)]
-    [InlineData(1, 2, 3)]
-    [InlineData(10, 10, 20)]
-    public void Sum_ShouldSumTwoValues_WhenInvoked(int a, int b, int expectedResult)
-    {
-        // Arrange
-        SomeFunctions _sut = new SomeFunctions();
-    
-        // Act
-        var result = _sut.Sum(a, b);
-    
-        // Assert
-        result.Should().Be(expectedResult);
-    }
-
     [Fact]
-    public void User_ShouldBeEqual_WhenInvoked()
+    public void UserList_ShouldNotBeEmpty_WhenHaveElements()
     {
         // Arrange
-        User _sut = new User
-        {
-            Id = 1,
-            Name = "João da Silva"
-        };
-    
         User user = new User
         {
             Id = 1,
             Name = "João da Silva"
         };
-    
+        var _sut = new List<User> { user };
+
         // Act
-    
+
         // Assert
-        _sut.Should().BeEquivalentTo(user);
+        _sut.Should().NotBeEmpty();
+        _sut.Should().HaveCount(1);
+        // _sut.Should().HaveCountGreaterThan(1);
+        // _sut.Should().HaveCountLessThan(1);
     }
 
     [Fact]
-    public void User_ShouldBeValid_WhenInvoked()
+    public void UserList_ShouldContainUser_WhenListIsNotEmpty()
     {
         // Arrange
-        User _sut = new User
+        User user = new User
         {
             Id = 1,
             Name = "João da Silva"
         };
+        var _sut = new List<User> { user };
+
+        // Act
+
+        // Assert
+        _sut.Should().NotBeEmpty();
+        _sut.Should().Contain(user);
+    }
+
+    [Fact]
+    public void UserList_ShouldContainUser_WhenListIsNotEmptyEquivalent()
+    {
+        // Arrange
+        User userToFind = new User
+        {
+            Id = 1,
+            Name = "João da Silva"
+        };
+        User user = new User
+        {
+            Id = 1,
+            Name = "João da Silva"
+        };
+        var _sut = new List<User> { user };
+
+        // Act
+
+        // Assert
+        _sut.Should().NotBeEmpty();
+        _sut.Should().ContainEquivalentOf(userToFind);
+    }
+
+    [Fact]
+    public void UserList_ShouldContainUserByName_WhenListIsNotEmpty()
+    {
+        // Arrange
+        User user = new User
+        {
+            Id = 1,
+            Name = "João da Silva"
+        };
+        var _sut = new List<User> { user };
     
         // Act
     
         // Assert
-        _sut.Id.Should().BeGreaterThan(0);
-        _sut.Name.Should().NotBeEmpty().And.Contain("João");
+        _sut.Should().Contain(x => x.Name.StartsWith("João"));
+    }
+
+    [Fact]
+    public void Divide_ShouldThrowException_WhenDividedByZero()
+    {
+        // Arrange
+        SomeFunctions _sut = new SomeFunctions();
+    
+        // Act
+        Func<int> result = () => _sut.Divide(1, 0);
+    
+        // Assert
+        result.Should().Throw<DivideByZeroException>();
     }
 }
